0b233984359f2f21c16fa25f75f1f7033575e546
Moises E Jaramillo <moisesj@outlook.com>
2024-10-07 23:21:37 -0400

    Expanded on tests

--- contracts/DentityVerificationsOracle.sol
+++ contracts/DentityVerificationsOracle.sol
@@ -7,16 +7,25 @@ import "./IDentityClient.sol";
 contract DentityVerificationsOracle {
     address payable public owner;
 
+    address[] private _trustedOracleNodes;
+
     // Event emitted to signal the Oracle Node to fetch the verification data
     event DentityVerificationRequested(string ensName, address clientContract);
 
     // Dictionary of verification requests keyed on the client contract address
-    mapping(address => VerificationRequest) _verificationRequests;
+    mapping(address => VerificationRequest) private _verificationRequests;
 
     struct VerificationRequest {
         string ensName;
         address callerContract;
         bool isPending;
+    }
+
+    struct VerificationResponse {
+        string ensName;
+        string errorCode;
+        string verifiablePresent;
+        address callerContract;
     }
 
     /*
@@ -36,25 +45,38 @@ contract DentityVerificationsOracle {
         return false;
     }*/
 
+    // TODO: Add method to add trusted oracles. Only the owner can add trusted oracles
+
     constructor() payable {
         // Ensure the deployer is the same as the account that will collect the funds
         owner = payable(msg.sender);
+    }
+
+    function addTrustedOracle(address oracleNodeAddress) public {
+        require(msg.sender == owner, "Only the owner can add trusted oracles");
+
+        require(oracleNodeAddress != address(0),
+            "Oracle Node cannot have a zero address"
+        );
+
+        _trustedOracleNodes.push(oracleNodeAddress);
     }
 
     function requestDentityVerification(
         string calldata ensName,
         address clientContract
     ) public payable {
+        // TODO: Check for parameters
         /*
+        // TODO: Check that the contract implements the IDentityClient interface
         require(
             isValidClient(clientContract),
             "Contract must implement IDentityClient"
-        );
+        );*/
 
         // TODO: Check that the fee is enough
-        // TODO: Check that the contract implements the IDentityClient interface
-        // TODO: Escrow fees
-        */
+
+        // TODO: Only then escrow fees and create the request
 
         VerificationRequest memory request = VerificationRequest(
             ensName,
@@ -65,5 +87,30 @@ contract DentityVerificationsOracle {
 
         // Emit event to signal the Oracle Node to fetch the verification data
         emit DentityVerificationRequested(ensName, clientContract);
+    }
+
+    function processOracleNodeResponse(VerificationResponse memory response) public view {
+
+        // TODO: Add checks on the caller and make sure we trust it
+
+        // Does request exist
+        if (bytes(_verificationRequests[response.callerContract].ensName).length != 0) {
+            VerificationRequest storage request = _verificationRequests[response.callerContract];
+
+            if (
+                request.isPending &&
+                keccak256(abi.encodePacked(response.errorCode)) ==
+                keccak256(abi.encodePacked("0"))
+            ) {
+                // Store the verification data in the client contract
+            } else {
+                // TODO: Refund the client
+                // Raise an event to signal the client contract
+            }
+        }
+    }
+
+    function getTrustedOracleNodes() external view returns (address[] memory) {
+        return _trustedOracleNodes;
     }
 }
--- test/DentityVerificationsOracle.ts
+++ test/DentityVerificationsOracle.ts
@@ -1,21 +1,15 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
 
+const CONTRACT_NAME = "DentityVerificationsOracle";
+
 describe("DentityVerificationsOracle", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployOracle() {
-    // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await hre.viem.getWalletClients();
 
-    const contract = await hre.viem.deployContract(
-      "DentityVerificationsOracle"
-    );
+    // Contracts are deployed using the first signer/account by default
+    const contract = await hre.viem.deployContract(CONTRACT_NAME);
 
     const publicClient = await hre.viem.getPublicClient();
 
@@ -29,7 +23,7 @@ describe("DentityVerificationsOracle", function () {
 
   describe("Deployment", function () {
     it("Deployer must be the owner that will get compensated", async function () {
-      const { contract, owner } = await deployOracle();
+      const { contract, owner } = await loadFixture(deployOracle);
 
       expect((await contract.read.owner()).toUpperCase()).to.equal(
         owner.account.address.toUpperCase()
@@ -37,9 +31,45 @@ describe("DentityVerificationsOracle", function () {
     });
   });
 
-  describe("Events", function () {
+  describe("Oracle Nodes", function () {
+    it("Should not add trusted oracle", async function () {
+      const { contract, otherAccount } = await loadFixture(deployOracle);
+
+      // We retrieve the contract with a different account to send a transaction
+      const otherAccountContract = await hre.viem.getContractAt(
+        CONTRACT_NAME,
+        contract.address,
+        { client: { wallet: otherAccount } }
+      );
+
+      await expect(
+        otherAccountContract.write.addTrustedOracle([
+          otherAccount.account.address,
+        ])
+      ).to.be.rejectedWith("Only the owner can add trusted oracles");
+    });
+
+    it("Add trusted oracle", async function () {
+      const { contract, owner, otherAccount } = await loadFixture(deployOracle);
+
+      // No failures expected
+      await contract.write.addTrustedOracle([otherAccount.account.address]);
+
+      // Check the oracle was added
+      const trustedOracles = await contract.read.getTrustedOracleNodes();
+
+      expect(
+        trustedOracles.filter(
+          (address) =>
+            address.toLowerCase() === otherAccount.account.address.toLowerCase()
+        )
+      ).to.have.lengthOf(1);
+    });
+  });
+
+  describe("Process Requests", function () {
     it("DentityVerificationRequested Event raised", async function () {
-      const { contract } = await deployOracle();
+      const { contract } = await loadFixture(deployOracle);
       const ensName = "moisesj.eth";
 
       await contract.write.requestDentityVerification([
@@ -47,7 +77,24 @@ describe("DentityVerificationsOracle", function () {
         contract.address,
       ]);
 
-      // get the withdrawal events in the latest block
+      // Get the raised events in the latest block
+      const events = await contract.getEvents.DentityVerificationRequested();
+
+      expect(events).to.have.lengthOf(1);
+      expect(events[0].args.ensName).to.equal(ensName);
+    });
+  });
+
+  describe("Process Responses", function () {
+    it("processOracleNodeResponse", async function () {
+      const { contract } = await loadFixture(deployOracle);
+      const ensName = "moisesj.eth";
+
+      await contract.write.requestDentityVerification([
+        ensName,
+        contract.address,
+      ]);
+
       const events = await contract.getEvents.DentityVerificationRequested();
       expect(events).to.have.lengthOf(1);
       expect(events[0].args.ensName).to.equal(ensName);
