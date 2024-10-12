e938211567021488a7c439c68be1db80b6505d36
Moises E Jaramillo <moisesj@outlook.com>
2024-10-11 23:27:02 -0400

    Tested Task with other smart contract

--- oracle-node/src/abi/test-dentity-client-abi.ts
+++ oracle-node/src/abi/test-dentity-client-abi.ts
@@ -1,70 +1,69 @@
 const testDentityClientAbi = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "address",
-        name: "oracleContractAddress",
-        type: "address",
-      },
+        "internalType": "address",
+        "name": "oracleContractAddress",
+        "type": "address"
+      }
     ],
-    stateMutability: "nonpayable",
-    type: "constructor",
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "string",
-        name: "ensName",
-        type: "string",
-      },
+        "internalType": "string",
+        "name": "ensName",
+        "type": "string"
+      }
     ],
-    name: "invokeOracle",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
+    "name": "invokeOracle",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "errorCode",
-        type: "uint256",
+        "internalType": "uint256",
+        "name": "errorCode",
+        "type": "uint256"
       },
       {
-        internalType: "string",
-        name: "credential",
-        type: "string",
+        "internalType": "string",
+        "name": "credential",
+        "type": "string"
       },
       {
-        internalType: "bool",
-        name: "isGated",
-        type: "bool",
-      },
+        "internalType": "bool",
+        "name": "isGated",
+        "type": "bool"
+      }
     ],
-    name: "processVerificationResult",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    "name": "processVerificationResult",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "bytes4",
-        name: "interfaceId",
-        type: "bytes4",
-      },
+        "internalType": "bytes4",
+        "name": "interfaceId",
+        "type": "bytes4"
+      }
     ],
-    name: "supportsInterface",
-    outputs: [
+    "name": "supportsInterface",
+    "outputs": [
       {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
     ],
-    stateMutability: "pure",
-    type: "function",
-  },
+    "stateMutability": "pure",
+    "type": "function"
+  }
 ];
-
 export default testDentityClientAbi;
